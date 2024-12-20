b303fc13fec7670325ca9216a9c3c6b0ba8eddde
IsDevOps <issyoseghale@gmail.com>
2024-12-20 16:48:49 +0100

    committed

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,18 +1,18 @@
 module "vpc" {
-  source       = "./terraform/modules/vpc"
+  source       = "./modules/vpc"
   cidr_block   = "10.0.0.0/16"
   subnet_count = 2
 }
 
 module "ecs" {
-  source            = "./terraform/modules/ecs"
+  source            = "./modules/ecs"
   container_image   = "oseghale1/flask-app:latest"
   subnet_ids        = module.networking.subnet_ids
   security_group_id = module.networking.ecs_security_group_id
 }
 
 module "alb" {
-  source            = "./terraform/modules/alb"
+  source            = "./modules/alb"
   vpc_id            = module.networking.vpc_id
   subnet_ids        = module.networking.subnet_ids
   security_group_id = module.networking.ecs_security_group_id
