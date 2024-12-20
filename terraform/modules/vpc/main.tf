09437fe18598ef784dfe3fae7cd427950259add5
IsDevOps <issyoseghale@gmail.com>
2024-12-20 23:56:57 +0100

    test

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -3,7 +3,6 @@ resource "aws_vpc" "main" {
 }
 
 resource "aws_subnet" "public_subnets" {
-  name              = "Public-subnet"
   count = 2
   vpc_id            = aws_vpc.main.id
   cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
@@ -21,14 +20,12 @@ resource "aws_route_table" "public" {
 }
 
 resource "aws_route" "internet_access" {
-  name                   = "ecs-route-table"
   route_table_id         = aws_route_table.public.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.main.id
 }
 
 resource "aws_route_table_association" "public" {
-  name           = "public-route-table"
   count          = length(aws_subnet.public_subnets)
   subnet_id      = aws_subnet.public_subnets[count.index].id
   route_table_id = aws_route_table.public.id
