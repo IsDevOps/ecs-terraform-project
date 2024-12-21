af7bba0b8349ee0f7673ac84af583cb48c5c5ea9
IsDevOps <issyoseghale@gmail.com>
2024-12-21 09:08:59 +0100

    commit

--- terraform/modules/security_groups/security_groups.tf
+++ terraform/modules/security_groups/security_groups.tf
@@ -5,8 +5,8 @@ resource "aws_security_group" "ecs_sg" {
   description = "Allow ECS task traffic"
 
   ingress {
-    from_port   = 80
-    to_port     = 80
+    from_port   = 5000
+    to_port     = 5000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
