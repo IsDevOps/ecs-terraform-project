b6a01b668fa0b2fe3b6e0cc695266467d98f615a
IsDevOps <issyoseghale@gmail.com>
2024-12-21 03:02:57 +0100

    committed

--- terraform/modules/alb/main.tf
+++ terraform/modules/alb/main.tf
@@ -10,6 +10,14 @@ resource "aws_alb_target_group" "main" {
   protocol = "HTTP"
   vpc_id   = var.vpc_id
   target_type = "ip"
+
+  health_check {
+    path                = "/health"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 3
+  }
 }
 
 
