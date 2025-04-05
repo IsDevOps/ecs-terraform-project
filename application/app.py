14fb86ea520fbe0cc6a3897958100c358573f18f
IsDevOps <issyoseghale@gmail.com>
2025-04-05 16:45:05 +0100

    sample deployment for migration to AWS

--- application/app.py
+++ application/app.py
@@ -1,12 +1,10 @@
-from flask import Flask, render_template
-from datetime import datetime
+import datetime
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
-def landing_page():
-    server_time = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
-    return render_template("index.html", server_time=server_time)
-
+def server_time():
+    return {"server_time": datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")}
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
