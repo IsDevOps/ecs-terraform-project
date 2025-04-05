05fcf413aaecaa2b23371d885480014de3765492
IsDevOps <issyoseghale@gmail.com>
2025-04-05 15:40:54 +0100

    sample application deployment
    Former-commit-id: f2df928f04483b97517493948cc4d297453a5e2d

--- application/app.py
+++ application/app.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route("/")
-def server_time():
-    return {"server_time": datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")}
+def landing_page():
+    server_time = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
+    return render_template("index.html", server_time=server_time)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
